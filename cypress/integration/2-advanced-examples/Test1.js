80a3c3ef980b8cd623fcdf0b1e963b503c3eba41
Ratneshwar <jharatnesh123@gmail.com>
2022-04-20 15:59:51 +0530

    first commit

---
+++ cypress/integration/2-advanced-examples/Test1.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('My first TEst suit', function()
+{
+    it('My first test case', function()
+    {
+        cy.visit("https://www.google.com/")
+    })
+})
